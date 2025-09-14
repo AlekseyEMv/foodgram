78e5651471fc8c2fdab1f1795eab0e43363190b2
Aleksey Molostov <lelik.ne@mail.ru>
2025-09-14 13:04:25 +0300

    Del mistake

--- backend/api/views.py
+++ backend/api/views.py
@@ -10,7 +10,7 @@ from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.mixins import CreateModelMixin, DestroyModelMixin
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permisions import AllowAny
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
